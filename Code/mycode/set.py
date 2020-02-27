cfa7f6e3115d649da21ec6ac1f6a76038f530e0f
John Miner <john.douglas.miner.iv@gmail.com>
2020-02-27 12:41:43 -0800

    Set balancer, Not Very Efficiant

--- Code/mycode/set.py
+++ Code/mycode/set.py
@@ -1,48 +1,91 @@
 from binarytree import BinarySearchTree
+from queue import LinkedQueue
 import sys
 
 
 class Setree:
     def __init__(self, ls=None):
         self.tree = BinarySearchTree()
+        self.size = 0
         for item in ls:
             self.add(item)
 
     def add(self, item):
+        '''Add an item to the set. O(log(n))'''
         if self.contains(item) is False:
             self.tree.insert(item)
+            self._update_size()
 
     def contains(self, item):
+        '''Check if set contains item. O(log(n))'''
         return self.tree.contains(item)
 
     def remove(self, item):
+        '''Remove item from set or ValueError O(log(n))'''
         self.tree.delete(item)
+        self._update_size()
 
     def union(self, nset):
-        return Setree(self.in_order() + nset.in_order())
+        '''Create a new set with elements of both sets O()'''
+        nnset = self.tree
+        for item in nset._in_order():
+            nnset.add(item)
+        return nnset
 
     def intersection(self, nset):
-        items = nset.in_order()
+        '''Create a new set with elements that are in both sets O(n)'''
+        items = nset._in_order()
         return Setree([i for i in items if self.contains(i)])
 
     def is_subset(self, nset):
-        items = nset.in_order()
+        '''Check if each item in one set is in another set O(n)'''
+        items = nset._in_order()
         for i in items:
             if self.contains(i) is False:
                 return False
         return True
 
-    def in_order(self):
+    def _in_order(self):
+        """returns all of the values in a set in sorted order"""
         return self.tree.items_in_order()
 
-    def size(self):
-        return self.tree.size
+    def _update_size(self):
+        """Updates the size."""
+        self.size = self.tree.size
+
+    def bad_balance(self):
+        print('Previous height:')
+        print(self._height())
+        queue = LinkedQueue()
+        ls = self._in_order()
+        self._binary_sorter(ls, queue.enqueue)
+        self.tree = BinarySearchTree()
+        while queue.front() is not None:
+            self.add(ls[queue.dequeue()])
+        print('Balanced height:')
+        print(self._height())
+
+    def _binary_sorter(self, ls, visit, last=None, left=None, right=None):
+        if left is None:
+            left, right = 0, len(ls) - 1
+        midpoint = left + ((right-left)//2)
+        visit(midpoint)
+        if midpoint != last:
+            self._binary_sorter(ls, visit, midpoint, midpoint + 1, right)
+        if midpoint != last:
+            self._binary_sorter(ls, visit, midpoint, left, midpoint - 1)
+
+    def _height(self):
+        return self.tree.root.height()
+
 
 
 def main():
     set = Setree(sys.argv[1:])
-    print(set.size())
-    print(set.in_order())
+    # print(set.size)
+    print(set._in_order())
+    set.bad_balance()
+    print(set._in_order())
 
 
 if __name__ == '__main__':
--- Code/mycode/set_test.py
+++ Code/mycode/set_test.py
@@ -16,31 +16,31 @@ class SetTest(unittest.TestCase):
 
     def test_set_delete(self):
         set = Setree([8,7,9,4,5,23,15,87])
-        assert set.size() == 8
+        assert set.size == 8
         assert set.in_order() == [4,5,7,8,9,15,23,87]
         set.remove(8)
-        assert set.size() == 7
+        assert set.size == 7
         assert set.in_order() == [4,5,7,9,15,23,87]
         set.remove(87)
-        assert set.size() == 6
+        assert set.size == 6
         assert set.in_order() == [4,5,7,9,15,23]
         set.remove(15)
-        assert set.size() == 5
+        assert set.size == 5
         assert set.in_order() == [4,5,7,9,23]
         set.remove(7)
-        assert set.size() == 4
+        assert set.size == 4
         assert set.in_order() == [4,5,9,23]
         set.remove(9)
-        assert set.size() == 3
+        assert set.size == 3
         assert set.in_order() == [4,5,23]
         set.remove(4)
-        assert set.size() == 2
+        assert set.size == 2
         assert set.in_order() == [5,23]
         set.remove(5)
-        assert set.size() == 1
+        assert set.size == 1
         assert set.in_order() == [23]
         set.remove(23)
-        assert set.size() == 0
+        assert set.size == 0
         assert set.in_order() == []
 
     def test_set_intersection(self):
